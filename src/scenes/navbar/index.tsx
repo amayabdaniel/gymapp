2f5b86415e601eb45e317ddba367fad54dde68ad
Daniel Amaya B <daniel.amaya.buitrago@outlook.com>
2023-06-03 18:19:44 -0500

    first one

---
+++ src/scenes/navbar/index.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import Logo from "@/assets/Logo.png";
+
+type Props = {}
+const Navbar = (props: Props) => {
+ 
+  const flexBetween = "flex justify-between items-center"
+  return (
+    <div>index</div>
+  )
+}
+
+export default Navbar
