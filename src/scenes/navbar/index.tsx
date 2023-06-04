0bb0463966e266ff752da9e5f2df7e7a94e8a6cc
Daniel Amaya B <daniel.amaya.buitrago@outlook.com>
2023-06-03 19:36:56 -0500

    navbar

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,14 +1,56 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
+import Link from "@/scenes/navbar/Link";
 
-type Props = {}
-const Navbar = (props: Props) => {
- 
-  const flexBetween = "flex justify-between items-center"
+type Props = {
+  selectedPage: string;
+  setSelectedPage: (value: string) => void;
+};
+
+const Navbar = ({selectedPage, setSelectedPage}: Props) => {
+  const flexBetween = "flex justify-between items-center";
   return (
-    <div>index</div>
-  )
-}
+    <nav>
+      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+          <div className={`${flexBetween} w-full gap-16`}>
+            {/* Left side */}
+            <img src={Logo} alt="logo" />
+            {/* Right side */}
+            <div className={`${flexBetween} w-full`}>
+              <div className={`${flexBetween} gap-8 text-sm`}>
+                <Link
+                  page="Home"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Benefits"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Our classes"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Contact us"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+              </div>
+              <div className={`${flexBetween} gap-8`}>
+                <p>Sign In</p>
+                <button>Become a Member</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
